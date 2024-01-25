9171d29379e7a80eed03a2d864b4c46b64e81cd6
unknown <lucall93@hotmail.com>
2024-01-25 14:44:13 -0300

    fixed bugs

--- config.db.js
+++ config.db.js
@@ -7,7 +7,7 @@ connection = mysql.createConnection({
     host:process.env.DBHOST,
     user:process.env.DBUSER,
     database:process.env.DBNAME,
-    password:''
+    password:process.env.DBPASS
 });
 
 connection.connect((error)=>{
