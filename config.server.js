d224315eb9c40d26907bc9e8cb4b15aca969e3fe
unknown <lucall93@hotmail.com>
2024-01-25 21:34:21 -0300

    clever url deploy

--- config.server.js
+++ config.server.js
@@ -1,7 +1,7 @@
 module.exports = {
     //serverUrl: '192.168.1.44',
     serverUrl: 'https://app-374dea39-79aa-43ce-86e5-5677f667e752.cleverapps.io',
-    socketUrl:'app-374dea39-79aa-43ce-86e5-5677f667e752.cleverapps.io',
+    socketUrl:'0.0.0.0',
     serverListenUrl:'0.0.0.0',
     serverPort: 8080,
   };
