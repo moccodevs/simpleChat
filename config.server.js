a4155ea15f3e0660bf4512bffc1edd2f3f5c0ce7
unknown <lucall93@hotmail.com>
2024-01-26 00:04:56 -0300

    clever url deploy

--- config.server.js
+++ config.server.js
@@ -1,6 +1,6 @@
 module.exports = {
     //serverUrl: '192.168.1.44',
-    serverUrl: 'https://app-374dea39-79aa-43ce-86e5-5677f667e752.cleverapps.io',
+    serverUrl: 'https://simplechat.cleverapps.io',
     serverListenUrl:'0.0.0.0',
     serverPort: 8080,
   };
